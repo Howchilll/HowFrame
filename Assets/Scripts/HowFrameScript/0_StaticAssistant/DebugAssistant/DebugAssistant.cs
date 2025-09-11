fac5a240c9052d2a00e8934211c5bd4d7206f81e
Howchilll <1527757792@qq.com>
2025-09-11 09:42:04 +0800

    Update DebugAssistant.cs

--- Assets/Scripts/HowFrameScript/0_StaticAssistant/DebugAssistant/DebugAssistant.cs
+++ Assets/Scripts/HowFrameScript/0_StaticAssistant/DebugAssistant/DebugAssistant.cs
@@ -1,49 +1,118 @@
 using System;
+using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
 
+public enum DebugColor
+{
+    White,
+    Red,
+    Green,
+    Blue,
+    Yellow,
+    Cyan,
+    Magenta,
+    Gray,
+    Black
+}
+
 public static class DebugAssistant
 {
-    public static void Log(this object obj, string prefix = "")
+    private static readonly Dictionary<string, bool> TagFilter = new Dictionary<string, bool>();
+
+    public static void SetTag(string tag, bool enabled)
+    {
+        if (string.IsNullOrEmpty(tag)) return;
+
+        if (TagFilter.ContainsKey(tag))
+            TagFilter[tag] = enabled;
+        else
+            TagFilter.Add(tag, enabled);
+    }
+
+    private static bool IsTagEnabled(string tag)
+    {
+        if (string.IsNullOrEmpty(tag)) return true; // 空字符串不限制
+        if (!TagFilter.TryGetValue(tag, value: out var enabled))
+        {
+            TagFilter[tag] = true; // 默认开启
+            return true;
+        }
+        return enabled;
+    }
+
+
+    public static void Log(this object obj, string prefix = "", DebugColor color = DebugColor.White, string tag = "")
+        => LogInternal(obj, prefix, color, LogType.Log, tag);
+
+    public static void Warning(this object obj, string prefix = "", string tag = "")
+        => LogInternal(obj, prefix, DebugColor.Yellow, LogType.Warning, tag);
+
+    public static void Error(this object obj, string prefix = "", string tag = "")
+        => LogInternal(obj, prefix, DebugColor.Red, LogType.Error, tag);
+    
+
+    // ---------------- 内部实现 ----------------
+    private static void LogInternal(object obj, string prefix, DebugColor color, LogType type, string tag)
     {
 #if UNITY_EDITOR
+        if (!IsTagEnabled(tag)) return;
+
+        string result;
+
         if (obj == null)
         {
-            Debug.Log($"{prefix}<null>");
-            return;
+            result = $"{prefix}<null>";
         }
-        Type type = obj.GetType();
-        if (type.IsPrimitive || obj is string || obj is decimal)
+        else
         {
-            Debug.Log($"{prefix}{obj}");
-            return;
-        }
-        string result = $"{prefix}<{type.Name}>";
-        FieldInfo[] fields = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
-        foreach (FieldInfo field in fields)
-        {
-            object value = field.GetValue(obj);
-            result += $"\n  [Field] {field.Name} = {FormatValue(value)}";
-        }
-        PropertyInfo[] properties = type.GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
-        foreach (PropertyInfo property in properties)
-        {
-            if (property.GetIndexParameters().Length == 0 && property.CanRead)
+            Type typeInfo = obj.GetType();
+            if (typeInfo.IsPrimitive || obj is string || obj is decimal)
             {
-                object value;
-                try
+                result = $"{prefix}{obj}";
+            }
+            else
+            {
+                result = $"{prefix}<color={ToHtml(color)}><{typeInfo.Name}></color>";
+                FieldInfo[] fields = typeInfo.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+                foreach (FieldInfo field in fields)
                 {
-                    value = property.GetValue(obj);
+                    object value = field.GetValue(obj);
+                    result += $"\n  [Field] {field.Name} = {FormatValue(value)}";
                 }
-                catch
+
+                PropertyInfo[] properties = typeInfo.GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+                foreach (PropertyInfo property in properties)
                 {
-                    value = "<无法访问>";
+                    if (property.GetIndexParameters().Length == 0 && property.CanRead)
+                    {
+                        object value;
+                        try
+                        {
+                            value = property.GetValue(obj);
+                        }
+                        catch
+                        {
+                            value = "<无法访问>";
+                        }
+                        result += $"\n  [Prop ] {property.Name} = {FormatValue(value)}";
+                    }
                 }
-                result += $"\n  [Prop ] {property.Name} = {FormatValue(value)}";
             }
         }
 
-        Debug.Log(result);
+        switch (type)
+        {
+            case LogType.Warning:
+                Debug.LogWarning(result);
+                break;
+            case LogType.Error:
+                Debug.LogError(result);
+                break;
+            default:
+                Debug.Log(result);
+                break;
+        }
 #endif
     }
 
@@ -58,7 +127,24 @@ public static class DebugAssistant
         }
         else
         {
-            return $"<{type.Name}>"; // 不展开
+            return $"<{type.Name}>"; // 只展开一层
+        }
+    }
+
+    private static string ToHtml(DebugColor color)
+    {
+        switch (color)
+        {
+            case DebugColor.White: return "white";
+            case DebugColor.Red: return "red";
+            case DebugColor.Green: return "green";
+            case DebugColor.Blue: return "blue";
+            case DebugColor.Yellow: return "yellow";
+            case DebugColor.Cyan: return "cyan";
+            case DebugColor.Magenta: return "magenta";
+            case DebugColor.Gray: return "gray";
+            case DebugColor.Black: return "black";
+            default: return "white";
         }
     }
 }
